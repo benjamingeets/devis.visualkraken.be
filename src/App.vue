ba941cdc26482ba81b0e8e5237aa83ad90179d0c
Benjamin Geets <bgeets@icloud.com>
2021-02-07 21:40:58 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <transition name="slide" mode="out-in">
+   <div class="home">
+      <div class="accueil" v-show="etape === 1">
+        <input type="checkbox" name="" v-model="reponse.fonctionnalites.tchat" id="">
+        <p @click="suivant()">next</p>
+      </div>
+      <div class="question1" v-show="etape === 2">
+        tu veux un tchat? {{reponse.fonctionnalites.tchat}}
+        <p @click="suivant()">Suivant</p>
+        <p @click="precedent()"> Precedent</p>
+      </div>
+   </div>
+  </transition>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data(){
+    return{
+      etape:1,
+      reponse:{
+        besoin:"",
+        fonctionnalites:{
+          tchat:false,
+          contact:false,
+          form:false,
+          booking:false
+        }
+      }
+    }
+  },
+  methods:{
+    suivant(){
+      this.etape++
+    },
+    precedent(){
+      this.etape--
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.slide-enter, .slide-leave-to{
+  opacity: 0;
+}
+.slide-enter-active .slide-leave-active{
+  transition: all 1s ease;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
